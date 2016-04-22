3f963aabfbfd7b4beba03a1951671c34afed4ecb
hamahiru <onintza.garmendia@gmail.com>
2016-04-22 16:05:06 +0200

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -3,17 +3,23 @@ import urllib2
 
 stocks = []
 url = 'http://finance.yahoo.com/d/quotes.csv?s=AAPL+GOOG+GOOGL+MDT+YNDX+QIWI+BIIB+GILD+CBI+NKE+F+FISV&f=nl1kj'
-stock_info = [['AAPL',17,102.42],['GOOG',3,198],['GOOGL',7,198],['MDT',65,30.725],['YNDX',90,15.53],['QIWI',65,21.94],['BIIB',4,295.22],['GILD',22,96.465],['CBI',60,39.22],['NKE',20,63.14],['F',175,12.28],['FISV',15,88.01]]
+url_euro = 'http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1&s=USDEUR=X'
+stock_info = [['AAPL',17,98],['GOOG',3,162],['GOOGL',7,162],['MDT',65,23.19],['YNDX',90,13.98],['QIWI',65,19.70],['BIIB',4,276.47],['GILD',22,81.66],['CBI',60,34.29],['NKE',20,58.85],['F',175,9.43],['FISV',15,81.77]]
 response = urllib2.urlopen(url)
 csvreader = csv.reader(response)
 stocksinternet=list(csvreader)
 for stock1, stock2 in zip(stock_info, stocksinternet):
     stocks.append(stock1+stock2)
-print stocks
-
-
+sum_profit = 0
+response_euro = urllib2.urlopen(url_euro)
+csvreader_euro = csv.reader(response_euro)
+eurodolar_list = list(csvreader_euro)
+eurodolar = eurodolar_list[0][1]
+print(eurodolar)
 for stock in stocks:
     buy_price = stock[1] * float(stock[2])
-    price_now = stock[1] * float(stock[4])
+    price_now = stock[1] * float(stock[4]) * float(eurodolar)
     profit = price_now - buy_price
-    print (stock[0] + ": " +  str(profit))
+    sum_profit += profit
+    print (stock[0] + ": " +  str(profit) + " Precio: " + str(stock[4]) +" Max52: " + str(stock[5]) + (" Min52: " + str(stock[6])))
+print( "profit: " + str(sum_profit))
